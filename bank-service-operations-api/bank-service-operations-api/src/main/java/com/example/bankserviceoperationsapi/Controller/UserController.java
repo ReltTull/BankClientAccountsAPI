dd6cac2030c22a6a3a8b2e00a37b4485734c0ff9
ReltTull <roman3202@gmail.com>
2024-09-07 12:01:09 +0300

    UserController mapping annotation fixed

--- bank-service-operations-api/bank-service-operations-api/src/main/java/com/example/bankserviceoperationsapi/Controller/UserController.java
+++ bank-service-operations-api/bank-service-operations-api/src/main/java/com/example/bankserviceoperationsapi/Controller/UserController.java
@@ -7,9 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
-@RequestMapping("/ ")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
